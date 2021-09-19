6dab2513a795e093578697e88ae38b6cb7f5c21a
Berna <bernasurfpix@gmail.com>
2021-09-19 10:50:51 +1000

    Carousel Part 1 - add Wrapper and Map area

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,26 +1,33 @@
 import React, { useState } from 'react';
 import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
 
-import { Container, Search } from './styles';
+import { Wrapper, Container, Search, Logo, Map } from './styles';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
 
   return (
-    <Container>
-      <Search>
-        <img src={logo} alt="restaurant logo" />
-        <TextField outlined label="Search">
-          <Input
-            type="text"
-            value={inputValue}
-            onChange={(event) => setInputValue(event.target.value)}
-          />
-        </TextField>
-      </Search>
-    </Container>
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="restaurant logo" />
+          <TextField
+            outlined
+            label="Search Reastaurants"
+            trailingIcon={<MaterialIcon role="button" icon="search" />}>
+            <Input
+              type="text"
+              value={inputValue}
+              onChange={(event) => setInputValue(event.target.value)}
+            />
+          </TextField>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
   );
 };
 
