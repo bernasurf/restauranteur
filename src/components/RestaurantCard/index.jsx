1ba98888f55f2c937e06158f3f4fd52423e7a78a
Berna <bernasurfpix@gmail.com>
2021-09-29 15:30:02 +1000

    Part 2 - Add Restaurant Info Component

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-import { Restaurant } from './styles';
+import { Restaurant, RestaurantInfo, Title, RestAddress } from './styles';
 
-const RestaurantCard = () => <Restaurant />;
+const RestaurantCard = () => (
+  <Restaurant>
+    <RestaurantInfo>
+      <Title>Restaurant Name</Title>
+      <p>Rating</p>
+      <RestAddress>1/44 Cavil Avenue</RestAddress>
+    </RestaurantInfo>
+  </Restaurant>
+);
 
 export default RestaurantCard;
